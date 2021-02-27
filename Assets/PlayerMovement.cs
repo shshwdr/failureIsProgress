32bc9eab4a81deed12f4b71c7129358fc4df89ce
shshwdr <cs.vivian04@gmail.com>
2021-02-26 18:30:10 -0600

    add tree

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController2D controller;
+
+    Animator animator;
+    public float runSpeed = 4f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        };
+        //if (Input.GetButtonDown("Crouch"))
+        //{
+        //    crouch = true;
+        //}
+        //else if (Input.GetButtonUp("Crouch"))
+        //{
+        //    crouch = false;
+        //};
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ Assets/VerticalPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatform : MonoBehaviour
+{
+    PlatformEffector2D effector;
+    float waitTime = 0.3f;
+    float currentWaitTimer = 0f;
+    GameObject playerCollider;
+    Collider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+        playerCollider = GameObject.Find("seed");
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow)|| Input.GetKeyDown(KeyCode.S))
+        {
+            collider.enabled = false;
+            //playerCollider.layer = LayerMask.NameToLayer("PlayerIgnoreBranch");
+            Invoke("changeLayerBack", 0.5f);
+            //effector.rotationalOffset = 180f;
+            //currentWaitTimer = waitTime;
+        }
+        //if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
+        //{
+        //    if (currentWaitTimer <= 0)
+        //    {
+
+        //    }
+        //}
+    }
+    void changeLayerBack()
+    {
+
+        collider.enabled = true;
+        //playerCollider.layer = LayerMask.NameToLayer("Player");
+    }
+}
